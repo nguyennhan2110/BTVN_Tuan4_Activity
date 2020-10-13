6222bf1d91a5035698f1374e63445526d8265c7e
nguyennhan2110 <nguyen.nhan211020@gmail.com>
2020-10-13 17:10:37 +0700

    system

---
+++ app/src/main/java/btvn/nhannguyen/MainActivity.java
@@ -0,0 +1,32 @@
+package btvn.nhannguyen;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn_back= (Button)
+                findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ChildActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+}
+
